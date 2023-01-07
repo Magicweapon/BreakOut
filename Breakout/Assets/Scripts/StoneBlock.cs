606e7ff6087463696dd9c63ed781f4eff68f9a50
Magicweapon <s_tar_craf_t@hotmail.com>
2023-01-06 19:08:52 -0600

    Project creation
    - Project's been created

---
+++ Breakout/Assets/Scripts/StoneBlock.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneBlock : Block
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        resistance = 5;
+    }
+}
---
+++ Breakout/Assets/Scripts/WoodBlock.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodBlock : Block
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        resistance = 3;
+    }
+
+    public override void BounceBall()
+    {
+        base.BounceBall();
+    }
+}
